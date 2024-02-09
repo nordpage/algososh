1168ad3ed2611e751da3c2d66d6d00d2745cb479
aurilaus <akhor@nordpage.ru>
2024-02-09 20:25:17 +0200

    update stack-page.tsx animation after Review

--- src/components/stack-page/stack-page.tsx
+++ src/components/stack-page/stack-page.tsx
@@ -1,4 +1,4 @@
-import React, {useEffect, useState} from "react";
+import React, {useEffect, useRef, useState} from "react";
 import {SolutionLayout} from "../ui/solution-layout/solution-layout";
 import styles from "./stack-page.module.css"
 import {Input} from "../ui/input/input";
@@ -7,35 +7,48 @@ import {Circle} from "../ui/circle/circle";
 import {ElementStates} from "../../types/element-states";
 import {SHORT_DELAY_IN_MS} from "../../constants/delays";
 import Stack from "../../classes/Stack";
+import {Animator} from "../common/animator";
+import {StackPushAnimator} from "./animators/StackPushAnimator";
+import {StackPopAnimator} from "./animators/StackPopAnimator";
 
 export const StackPage: React.FC = () => {
   const [data, setData] = useState<string>("");
-  const [stack, setStack] = useState(new Stack<string>())
+  const stack = useRef(new Stack<string>())
   const [submitted, setSubmitted] = useState<boolean>(false)
+  const [display, setDisplay] = useState<string[]>([])
+  const animator = useRef<Animator<Stack<string>, string>>();
 
 
   useEffect(() => {
-    if (submitted) {
-      setTimeout(() => {
-        setData("")
-        setSubmitted(false)
-      }, SHORT_DELAY_IN_MS)
+    if (!submitted) return;
+    let handle: number | undefined;
+    const animate = () => {
+      const result = animator.current!.animateStep();
+
+      setDisplay([...result.result.items]);
+      if (!result.completed) handle =  window.setTimeout(animate, SHORT_DELAY_IN_MS); else {
+        setSubmitted(false);
+        setData("");
+      }
     }
+    handle = window.setTimeout(animate, SHORT_DELAY_IN_MS);
+    return () => window.clearTimeout(handle);
   }, [submitted]);
 
   function addValue() {
-    stack.enqueue(data)
+    animator.current = new StackPushAnimator<string>(stack.current,data);
     setSubmitted(true)
   }
 
   function deleteValue() {
-    stack.dequeue()
+    animator.current = new StackPopAnimator<string>(stack.current);
     setSubmitted(true)
   }
 
   function clear() {
-    stack.peek()
+    stack.current.peek()
     setSubmitted(true)
+    setDisplay([])
   }
 
   return (
@@ -45,26 +58,19 @@ export const StackPage: React.FC = () => {
           <div className={styles.HContainer}>
             <Input maxLength={4} max={4} value={data} onChange={(e) => setData(e.currentTarget.value)} extraClass={styles.input}/>
             <Button text="Добавить" disabled={data === ""} onClick={addValue} extraClass={styles.btn}/>
-            <Button text="Удалить" disabled={stack.isEmpty()} onClick={deleteValue} extraClass={styles.btn}/>
+            <Button text="Удалить" disabled={display.length === 0} onClick={deleteValue} extraClass={styles.btn}/>
           </div>
           <div className={styles.HContainer}>
-            <Button text="Очистить" disabled={stack.isEmpty()} onClick={clear} extraClass={styles.btn}/>
+            <Button text="Очистить" disabled={display.length === 0} onClick={clear} extraClass={styles.btn}/>
           </div>
         </div>
         <div className={styles.HContainer}>
           {
-            !stack.isEmpty() && stack.items.map((value, index) => {
-                const head = stack.isHead(value) ? "Top" : ""
-                const computeState = () => {
-                  if (index === stack.size() - 1) {
-                    return submitted ? ElementStates.Changing : ElementStates.Default
-                  } else {
-                    return ElementStates.Default
-                  }
-                }
-                const targetState = computeState();
+            display.map((value, index) => {
 
-                return <Circle key={index} letter={value} head={head} index={index} state={targetState}/>
+                const targetState = submitted ? animator.current?.getStatus(index, value) : ElementStates.Default;
+
+                return <Circle key={index} letter={value} head={index === 0 ? "Top" : ""} index={index} state={targetState}/>
               })
           }
         </div>
